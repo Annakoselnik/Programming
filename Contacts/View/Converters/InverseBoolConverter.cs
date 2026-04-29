6951426cf2f04a51a7123abbbbdcac34ae94036f
annaKoshelnik <annakoselnik322@gmail.com>
2026-04-29 14:36:16 +0700

    /

--- Contacts/View/Converters/InverseBoolConverter.cs
+++ Contacts/View/Converters/InverseBoolConverter.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 using System.Windows.Data;
 
 namespace View.Converters
-{
+{ 
     public class InverseBoolConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture) =>
